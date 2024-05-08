96027e574968f9cb939561c1b75e2fe21a3103d9
Dario Jularic <dariojularic0@gmail.com>
2024-05-08 23:12:30 +0200

    Create calculator function

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,13 @@
 import './style.css'
+
+function calculatorFactory() {
+  let result;
+  let number;
+
+  const getResult = () => result;
+  const setResult = (result, operation, number) => result, operation, number
+
+  // const calculateOperation = () =>
+
+
+}
