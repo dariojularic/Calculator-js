c3a503d9865e715a25218fba4bd63a66a3f6c79c
Dario Jularic <dariojularic0@gmail.com>
2024-05-09 17:30:12 +0200

    Refactor factory function

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,26 +1,60 @@
 import './style.css'
 
-const numberBtn = document.querySelectorAll(".number");
+const buttons = document.querySelector(".buttons")
+const numberButtons = document.querySelectorAll(".number");
 const clearBtn = document.querySelector(".clear");
 const deleteBtn = document.querySelector(".delete");
 const dotBtn = document.querySelector(".dot");
 const equalBtn = document.querySelector(".equal");
 const operationBtn = document.querySelector(".operation");
+const displayPrimary = document.querySelector(".primary-output");
+const displaySecondary = document.querySelector(".secondary-output");
 
 
 function calculatorFactory() {
-  let result;
-  let number;
+  let secondNumber = "";
+  let firstNumber = "";
+  let operation = "";
 
-  const getResult = () => result;
+  const getFirstNumber = () => firstNumber;
+  const setFirstNumber = (firstNum) => firstNumber += firstNum;
+  const resetFirstNumber = () => firstNumber = "";
 
-  const getNumber = () => number;
-  const setNumber = (num) => number = num;
+  const getSecondNumber = () => secondNumber;
+  const setSecondNumber = (secondNum) => secondNumber += secondNum;
+  const resetSecondNumber = () => secondNumber = "";
 
+  const getOperation = () => operation;
+  const setOperation = (mathOperation) => operation = mathOperation;
 
-
-
+  return {getFirstNumber, setFirstNumber, resetFirstNumber, getSecondNumber, setSecondNumber, getOperation, setOperation}
   // const setResult = (result, operation, number) => result, operation, number
-
-  // const calculateOperation = () =>
 }
+
+const addition = (x, y) => x + y;
+const subtraction = (x, y) => x - y;
+const multiplication = (x, y) => x * y;
+const division = (x, y) => x + y;
+
+const calculator = calculatorFactory();
+
+
+buttons.addEventListener("click", (event) => {
+  if (event.target.classList.contains("number")) {
+    calculator.setFirstNumber(event.target.value)
+    displayPrimary.textContent = "";
+    displayPrimary.textContent += calculator.getFirstNumber();
+  }
+
+  if (event.target.classList.contains("operation") && calculator.getFirstNumber() !== "" && calculator.getSecondNumber() === "") {
+    calculator.setSecondNumber(calculator.getFirstNumber());
+    calculator.resetFirstNumber();
+    calculator.setOperation(event.target.value)
+    displayPrimary.textContent = "";
+    displaySecondary.textContent = `${calculator.getSecondNumber()} ${calculator.getOperation()}`;
+  }
+
+  if (event.target.classList.contains("operation") && calculator.getSecondNumber() !== "" && calculator.getFirstNumber() !== "") {
+
+  }
+})
