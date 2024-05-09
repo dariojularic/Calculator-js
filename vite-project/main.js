5189452105e156cda8e9db76b9c09cbe559efc79
Dario Jularic <dariojularic0@gmail.com>
2024-05-09 13:00:57 +0200

    Add query selectors

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,13 +1,26 @@
 import './style.css'
 
+const numberBtn = document.querySelectorAll(".number");
+const clearBtn = document.querySelector(".clear");
+const deleteBtn = document.querySelector(".delete");
+const dotBtn = document.querySelector(".dot");
+const equalBtn = document.querySelector(".equal");
+const operationBtn = document.querySelector(".operation");
+
+
 function calculatorFactory() {
   let result;
   let number;
 
   const getResult = () => result;
-  const setResult = (result, operation, number) => result, operation, number
+
+  const getNumber = () => number;
+  const setNumber = (num) => number = num;
+
+
+
+
+  // const setResult = (result, operation, number) => result, operation, number
 
   // const calculateOperation = () =>
-
-
 }
