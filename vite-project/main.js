9ba70cfcd86f371c9f68182225578d15cffcc9f5
Dario Jularic <dariojularic0@gmail.com>
2024-05-21 22:26:40 +0200

    Add comment

--- vite-project/main.js
+++ vite-project/main.js
@@ -41,7 +41,7 @@ function calculatorFactory() {
 }
 const calculator = calculatorFactory();
 // odavje nadalje je sve isto
-// 192 do 198 sve pogledat jonasaa
+// 192 do 198 sve pogledat jonasaaa
 
 function clearPrimaryDisplay() {
   displayPrimary.textContent = "";
